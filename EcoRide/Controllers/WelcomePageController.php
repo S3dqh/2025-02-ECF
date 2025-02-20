48a88399c67fd1ceb61c9b4a69d0377c7f8f86e8
S3dqh <jeremy.fer@laposte.net>
2025-02-20 21:42:01 +0100

    Delete EcoRide directory

--- EcoRide/Controllers/WelcomePageController.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define("HEADER_CONTENT", "Page d'accueil");
-    require_once "ecoride/views/header.php";
-    require_once "ecoride/views/welcome.php";
-    require_once "ecoride/views/footer.php";
-?>
